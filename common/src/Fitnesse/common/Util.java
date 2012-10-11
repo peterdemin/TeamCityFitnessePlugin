199dd5a77550ad27e7aab488671e9c40f1fee39f
elgris <iddqd07@yandex.ru>
2012-10-11 18:26:45 +0400

    I totally forgot to commit shared constants... :( Let's fix it :)

--- common/src/Fitnesse/common/Util.java
+++ common/src/Fitnesse/common/Util.java
@@ -1,16 +1,14 @@
 package Fitnesse.common;
 
 /**
- * Example server and agent shared class
+ * Server and agent shared class with constants definition
  */
 public class Util {
-  public final static String NAME = "Fitnesse";
-  public final static String RUNNER_TYPE = "Fitnesse";
+    public final static String NAME = "Fitnesse";
+    public final static String RUNNER_TYPE = "Fitnesse";
 
 
-  public final static String PROPERTY_FITNESSE_PORT = "fitnessePort";
-  public final static String PROPERTY_FITNESSE_TEST = "fitnesseTest";
-
-
-
+    public final static String PROPERTY_FITNESSE_PORT = "fitnessePort";
+    public final static String PROPERTY_FITNESSE_TEST = "fitnesseTest";
+    public final static String PROPERTY_FITNESSE_HOST = "fitnesseHost";
 }
